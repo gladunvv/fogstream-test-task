a6f2eb0b9ef06c4bc4092e9be4f9f94485b91fbe
vladislav <bincha.1997@gmail.com>
2019-12-28 18:00:18 +0900

    Update settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -93,19 +93,23 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 # Test settings database
 
-if 'test' in sys.argv:
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        }
+# if 'test' in sys.argv:
+#     DATABASES['default'] = {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         }
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+    },
+    'postgres': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),  
         'PASSWORD': env('DB_PASS'),
         'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'),
+        'PORT': env('DB_PORT')
     }
 }
 
